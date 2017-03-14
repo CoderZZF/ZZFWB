184d8599a8a9b31185628b9ce6ea9a0c8375e7c6
zhangzhifu <595373131@qq.com>
2017-03-15 03:30:31 +0800

    项目框架搭建

---
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by zhangzhifu on 2017/3/15.
+//  Copyright © 2017年 seemygo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVc("Home")
+        addChildVc("Live")
+        addChildVc("Follow")
+        addChildVc("Profile")
+    }
+    
+    private func addChildVc(storyName : String) {
+        // 1. 通过storyboard获取控制器
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        // 2. 将childVc作为子控制器
+        addChildViewController(childVc)
+    }
+}
